d209b8b68303369028b4bc4fb1f8d7a1c99a5ede
Colin Binion <colin.binion@me.com>
2018-07-21 13:37:04 -0600

    final

--- index.js
+++ index.js
@@ -1,36 +1,117 @@
 const hapi = require('hapi');
 const mongoose = require('mongoose');
+const {
+  graphqlHapi,
+  graphiqlHapi
+} = require('apollo-server-hapi');
+const schema = require('./graphql/schema');
+const Painting = require('./models/Painting');
+
+/* swagger section */
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./package');
+
 
 const server = hapi.server({
   port: 4000,
   host: 'localhost'
 });
 
-mongoose.connect('mongo://bobLoblaw:test1234@ds145951.mlab.com:45951/modernapi');
-
-mongoose.connect('mongo://indrek::test@ds129670.mlab.com:31090/powerful-api');
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds145951.mlab.com:45951/modernapi');
 
 mongoose.connection.once('open', () => {
-  console.log('====================================');
-  console.log(`connected to DB`);
-  console.log('====================================');
-})
+  console.log('connected to database');
+});
 
 const init = async () => {
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-      return '<h1>My Hapi API is running';
+
+  await server.register([
+    Inert,
+    Vision,
+    {
+      plugin: HapiSwagger,
+      options: {
+        info: {
+          title: 'Paintings API Documentation',
+          version: Pack.version
+        }
+      }
+    }
+  ]);
+
+  await server.register({
+    plugin: graphiqlHapi,
+    options: {
+      path: '/graphiql',
+      graphiqlOptions: {
+        endpointURL: '/graphql'
+      },
+      route: {
+        cors: true
+      }
     }
   });
 
+  await server.register({
+    plugin: graphqlHapi,
+    options: {
+      path: '/graphql',
+      graphqlOptions: {
+        schema
+      },
+      route: {
+        cors: true
+      }
+    }
+  });
+
+
+  server.route([{
+      method: 'GET',
+      path: '/api/v1/paintings',
+      config: {
+        description: 'Get all the paintings',
+        tags: ['api', 'v1', 'painting']
+      },
+      handler: (req, reply) => {
+        return Painting.find();
+      }
+    },
+    {
+      method: 'POST',
+      path: '/api/v1/paintings',
+      config: {
+        description: 'Get a specific painting by ID.',
+        tags: ['api', 'v1', 'painting']
+      },
+      handler: (req, reply) => {
+        const {
+          name,
+          url,
+          technique
+        } = req.payload;
+        const painting = new Painting({
+          name,
+          url,
+          technique
+        });
+
+        return painting.save();
+      }
+    }
+  ]);
+
   await server.start();
-  console.log('====================================');
   console.log(`Server running at: ${server.info.uri}`);
-  console.log('====================================');
 };
 
-
+process.on('unHandledRejection', (err) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+});
 
 init();
